79e99a5b617fdca7cc88888184ab40cdb42eda2c
Klesio <klesio.dev@gmail.com>
2022-02-27 20:38:53 -0300

    Error in server

--- 01-back-end-ssr/index.mjs
+++ 01-back-end-ssr/index.mjs
@@ -1,8 +1,29 @@
 import express from 'express';
 
 const app = express();
-
 app.set('view engine', 'ejs');
+
+/* app.get('/api/users', (req, res) => {
+  //buscava dado no banco
+
+  const users = [
+    {
+      id: 1,
+      name: 'Max',
+    },
+    {
+      id: 2,
+      name: 'Mayke',
+    },
+    {
+      id: 3,
+      name: 'Ana',
+    },
+  ];
+
+  //return res.render('users/list', {users});
+  return res.json({ data: users });
+}); */
 
 app.get('/users', (req, res) => {
   //buscava dado no banco
@@ -22,7 +43,11 @@ app.get('/users', (req, res) => {
     },
   ];
 
-  return res.render('users/list');
+  if (req.header('Accept') == 'application/json') {
+    return res.json({ data: users });
+  }
+
+  return res.render('users/list', { users });
 });
 
-app.listen(3333);
+app.listen(3335);
